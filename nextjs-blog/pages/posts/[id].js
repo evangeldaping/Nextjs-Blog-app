e90f2c880c9b05d12afc7ffbb0307cc5ba32683f
evangel <evangeldaping@gmail.com>
2021-10-04 22:00:59 +0800

    Imported Head and added the title tag by using post data.

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,5 +1,6 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
 
 export async function getStaticProps({ params }) {
   // Add the "await" keyword like this:
@@ -22,6 +23,9 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       {postData.title}
       <br />
       {postData.id}
